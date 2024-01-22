4f485c3f2255e3c1c004bd8ac2617e6ea3449dd8
暗里着迷 <642028114@qq.com>
2024-01-22 15:41:59 +0800

    封装kafka调用2

--- test/kafka_test.go
+++ test/kafka_test.go
@@ -9,12 +9,13 @@ import (
 	"github.com/sadlil/gologger"
 	"github.com/segmentio/kafka-go"
 	"runtime/debug"
+	"sync"
 	"testing"
 	"time"
 )
 
 var (
-	logger    = gologger.GetLogger()
+	logger    gologger.GoLogger
 	topicName = "dm_topic"
 )
 
@@ -26,9 +27,19 @@ type Member struct {
 	RegisterTime time.Time `json:"register_time"`
 }
 
+func init() {
+	logger = gologger.GetLogger(gologger.FILE, "logs/aaa.log")
+	logger.Info("开始测试了")
+}
+
+func TestInit(t *testing.T) {
+	//logger = gologger.GetLogger(gologger.FILE, "logs/aaa.log")
+	logger.Info(fmt.Sprintf("你唉唉唉:%s", t.Name()))
+}
+
 func TestRocketMqByConfig(t *testing.T) {
 	mc := model.Config{
-		Servers: []string{"192.168.20.130:9092"},
+		Servers: []string{"192.168.20.10:9092"},
 		ProductConfig: model.ProductConfig{
 			RetryCount:             2,
 			WriteTimeout:           1,
@@ -56,12 +67,13 @@ type testEventHandler struct{}
 var Test testEventHandler
 
 func (o *testEventHandler) handleExpireMsg(message kafka.Message) {
-	offset := message.Offset
-	fmt.Printf("接收到原始消息: time=%v, topic=%s, partition=%d, offset=%d, key=%s, message content=%s\n", message.Time, message.Topic, message.Partition, offset, string(message.Key), string(message.Value))
+	//offset := message.Offset
+	//fmt.Printf("接收到原始消息: time=%v, topic=%s, partition=%d, offset=%d, key=%s, message content=%s\n", message.Time, message.Topic, message.Partition, offset, string(message.Key), string(message.Value))
 	safeHandler(message, func(message kafka.Message) {
-		var m Member
-		_ = json.Unmarshal(message.Value, &m)
-		fmt.Println("打印序列化后消息：", m)
+		//var m Member
+		//_ = json.Unmarshal(message.Value, &m)
+		logger.Info(fmt.Sprintf("【%d】收到:%s", message.Offset, string(message.Value)))
+		//fmt.Println("打印序列化后消息：", m)
 	})
 }
 
@@ -90,26 +102,35 @@ func getCurTime() time.Time {
 
 func SendMessage() {
 	logger.Debug("kafka生产者发消息拉……")
-	for i := 0; i < 10; i++ {
-		m := Member{
-			Name:         fmt.Sprintf("吊毛666-%d", i),
-			Email:        fmt.Sprintf("diaomao-%d@qq.com", i),
-			Status:       true,
-			Id:           666 + i,
-			RegisterTime: getCurTime(),
-		}
-		mJson, _ := json.Marshal(m)
-		key := fmt.Sprintf("dm-1%d", i)
-		err := producer.ProducerClient.SendMessage(kafka.Message{
-			Topic: topicName,
-			Value: mJson,
-			Key:   []byte(key),
-		})
-		if err != nil {
-			logger.Error(fmt.Sprintf("%d: kafka发送消息失败", i))
-			return
-		}
-		time.Sleep(1 * time.Second)
+	wg := &sync.WaitGroup{}
+	ww := &sync.Mutex{}
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			m := Member{
+				Name:         fmt.Sprintf("吊毛666666-%d", i),
+				Email:        fmt.Sprintf("diaomao-%d@qq.com", i),
+				Status:       true,
+				Id:           666666 + i,
+				RegisterTime: getCurTime(),
+			}
+			mJson, _ := json.Marshal(m)
+			key := fmt.Sprintf("dm-1%d", i)
+			ww.Lock()
+			err := producer.ProducerClient.SendMessage(kafka.Message{
+				Topic: topicName,
+				Value: mJson,
+				Key:   []byte(key),
+			})
+			ww.Unlock()
+			if err != nil {
+				logger.Error(fmt.Sprintf("%d: kafka发送消息失败", i))
+				return
+			}
+		}(i)
+		//time.Sleep(1 * time.Second)
 	}
+	wg.Wait()
 	logger.Info("发送成功")
 }
